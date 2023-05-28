79ed4c40173eefa42454e4d3d7687b5dfa2d7d2e
Billy Kozma <wkozma@ntia.doc.gov>
2023-05-28 02:36:02 -0600

    added multiple elevation angle support

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -263,6 +263,74 @@ namespace P2108Comparer
             ResetPlotAxis();
         }
 
+        private void RenderMultipleElevationAngles()
+        {
+            // grab values from input control
+            var inputControl = grid_InputControls.Children[0] as MultipleElevationAnglesInputControl;
+            double f__ghz = inputControl.f__ghz;
+            var angles = inputControl.angles;
+            double h__meter = inputControl.h__meter;
+            var scenerio = inputControl.Scenerio;
+            var antenna = inputControl.Antenna;
+
+            // clear any current data on the plot
+            PlotModel.Series.Clear();
+
+            for (int i = 0; i < angles.Count; i++)
+            {
+                double theta__deg = angles[i];
+                var color = Constants.Colors[i % Constants.Colors.Length];
+
+                // set up line series
+                var p2108Series = new LineSeries()
+                {
+                    LineStyle = LineStyle.Solid,
+                    StrokeThickness = 2,
+                    Title = $"Rec P.2108-1, {theta__deg} deg",
+                    Color = color
+                };
+                var TEMP2Series = new LineSeries()
+                {
+                    LineStyle = LineStyle.Dot,
+                    StrokeThickness = 2,
+                    Title = $"Proposed Revision, {theta__deg} deg",
+                    Color = color
+                };
+
+                // generate curve data from 0.01% to 99.99%
+                for (double p = 0.01; p <= 99.99; p += 0.01)
+                {
+                    P2108.AeronauticalStatisticalModel(f__ghz, theta__deg, p, out double L_ces__db);
+                    p2108Series.Points.Add(new DataPoint(L_ces__db, p));
+
+                    TEMP2Model.AeronauticalStatisticalModel(f__ghz, theta__deg, p, h__meter, scenerio, antenna, out double L_clt__db);
+                    TEMP2Series.Points.Add(new DataPoint(L_clt__db, p));
+                }
+
+                // add series to plot
+                PlotModel.Series.Add(p2108Series);
+                PlotModel.Series.Add(TEMP2Series);
+
+                // update plot title
+                PlotModel.Title = $"Slant-Path Clutter Model Comparison at Multiple Elevation Angles";
+                PlotModel.Subtitle = $"freq = {f__ghz} GHz; h = {h__meter} meter; {scenerio} (h_s = {(int)scenerio} meter)";
+            }
+
+            // add legend to plot
+            PlotModel.Legends.Add(new Legend()
+            {
+                LegendPosition = OxyPlot.Legends.LegendPosition.BottomRight,
+                LegendPlacement = LegendPlacement.Inside,
+                LegendOrientation = LegendOrientation.Vertical,
+                LegendBorder = OxyColors.Black,
+                LegendBackground = OxyColors.White
+            });
+
+            // force plot redraw
+            plot.InvalidatePlot();
+            ResetPlotAxis();
+        }
+
         /// <summary>
         /// Exit the application
         /// </summary>
@@ -326,35 +394,11 @@ namespace P2108Comparer
                     userControl = new MultipleFrequenciesInputControl();
                     break;
 
-                /*case PlotMode.MultipleLowTerminals:
-                    Render = RenderMultipleLowHeights;
-                    Export = CsvExport_MultipleLowTerminals;
+                case PlotMode.MultipleElevationAngles:
+                    Render = RenderMultipleElevationAngles;
 
-                    userControl = new MultipleLowHeightsInputsControl();
-
-                    IsModeOfPropEnabled = false;
-                    IsModeOfPropChecked = false;
+                    userControl = new MultipleElevationAnglesInputControl();
                     break;
-
-                case PlotMode.MultipleHighTerminals:
-                    Render = RenderMultipleHighHeights;
-                    Export = CsvExport_MultipleHighTerminals;
-
-                    userControl = new MultipleHighHeightsInputsControl();
-
-                    IsModeOfPropEnabled = false;
-                    IsModeOfPropChecked = false;
-                    break;
-
-                case PlotMode.MultipleTimes:
-                    Render = RenderMultipleTimes;
-                    Export = CsvExport_MultipleTimePercentages;
-
-                    userControl = new MultipleTimeInputsControl();
-
-                    IsModeOfPropEnabled = false;
-                    IsModeOfPropChecked = false;
-                    break;*/
             }
 
             grid_InputControls.Children.Add(userControl);
---
+++ UserControls/MultipleElevationAnglesInputControl.xaml.cs
@@ -0,0 +1,125 @@
+﻿using P2108Comparer.PropModels;
+using P2108Comparer.ValidationRules;
+using P2108Comparer.Windows;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace P2108Comparer.UserControls
+{
+    public partial class MultipleElevationAnglesInputControl : UserControl, INotifyPropertyChanged
+    {
+        private int _errorCnt = 0;
+
+        /// <summary>
+        /// Frequencies, in GHz
+        /// </summary>
+        public ObservableCollection<double> angles { get; set; } = new ObservableCollection<double>() { 10 };
+
+        public double f__ghz { get; set; }
+
+        /// <summary>
+        /// Ground station height, in meter
+        /// </summary>
+        public double h__meter { get; set; }
+
+        /// <summary>
+        /// Clutter scenerio
+        /// </summary>
+        public TEMP2Model.Scenerio Scenerio { get; set; } = TEMP2Model.Scenerio.LowRise;
+
+        /// <summary>
+        /// Antenna type
+        /// </summary>
+        public TEMP2Model.Antenna Antenna { get; set; } = TEMP2Model.Antenna.Isotropic;
+
+        /// <summary>
+        /// Number of validation errors
+        /// </summary>
+        public int ErrorCnt
+        {
+            get { return _errorCnt; }
+            set
+            {
+                _errorCnt = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MultipleElevationAnglesInputControl()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+        }
+
+        private void TextBox_Error(object sender, ValidationErrorEventArgs e)
+        {
+            if (e.Action == ValidationErrorEventAction.Added)
+                ErrorCnt++;
+            else
+                ErrorCnt--;
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        private void Btn_Add_Click(object sender, RoutedEventArgs e)
+        {
+            var wndw = new AddFrequencyWindow();
+
+            if (!wndw.ShowDialog().Value)
+                return;
+
+            if (angles.Count == 0)
+            {
+                ErrorCnt--;
+                Validation.ClearInvalid(lb_angles.GetBindingExpression(ListBox.ItemsSourceProperty));
+            }
+
+            angles.Add(wndw.frequency);
+        }
+
+        private void Btn_Remove_Click(object sender, RoutedEventArgs e)
+        {
+            var itemsToRemove = new List<double>();
+
+            foreach (double item in lb_angles.SelectedItems)
+                itemsToRemove.Add(item);
+
+            foreach (var item in itemsToRemove)
+                angles.Remove(item);
+
+            if (angles.Count == 0)
+            {
+                ErrorCnt++;
+
+                var binding = lb_angles.GetBindingExpression(ListBox.ItemsSourceProperty);
+                var error = new ValidationError(new DoubleValidation(), binding) { ErrorContent = "At least 1 elevation angle is required" };
+                Validation.MarkInvalid(binding, error);
+            }
+        }
+
+        /// <summary>
+        /// Control if the 'Remove' button is enabled
+        /// </summary>
+        private void Lb_angles_SelectionChanged(object sender, SelectionChangedEventArgs e) =>
+            btn_Remove.IsEnabled = lb_angles.SelectedItems.Count > 0;
+    }
+}
